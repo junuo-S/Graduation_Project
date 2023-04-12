a2c37b8b5a3545575ff2513f8fe49cee05bf3898
junuo-S <3236877923@qq.com>
2023-04-12 16:44:25 +0800

    [日期:2023.4.12]-[项目:database]-[模块:tb_task]-[改动:创建计划任务数据表，并插入9条数据]

---
+++ database/tb_task_query.sql
@@ -0,0 +1,18 @@
+create table if not exists tb_task(
+    id int primary key auto_increment,
+    time int not null ,
+    robot_id varchar(20) not null ,
+    belt_id varchar(20) not null,
+    -- 0: 已完成 1: 进行中
+    status int default 1
+);
+drop table tb_task;
+insert into tb_task values (1, 1677483647, 'robot-1', 'belt-1', 0);
+insert into tb_task values (2, 1677583647, 'robot-2', 'belt-2', 0);
+insert into tb_task values (3, 1677683647, 'robot-3', 'belt-3', 0);
+insert into tb_task values (4, 1677783647, 'robot-1', 'belt-1', 0);
+insert into tb_task values (5, 1677883647, 'robot-2', 'belt-2', 0);
+insert into tb_task values (6, 1677983647, 'robot-3', 'belt-3', 0);
+insert into tb_task values (7, 1678083647, 'robot-1', 'belt-1', 0);
+insert into tb_task values (8, 1678183647, 'robot-2', 'belt-2', 0);
+insert into tb_task values (9, 1678283647, 'robot-3', 'belt-3', 0);

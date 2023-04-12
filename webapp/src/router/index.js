9d93e364cf75ca78b71f93b90005f1e1fe7a2caa
junuo-S <3236877923@qq.com>
2023-04-12 21:14:42 +0800

    [日期:2023.4.12]-[项目:webapp]-[模块:router]-[改动:添加巡检计划组件的路由]

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -4,6 +4,7 @@ import Login from "@/pages/Login.vue";
 import Home from "@/components/Home.vue";
 import UserManage from "@/pages/User-Manage.vue";
 import DeviceManage from "@/pages/Device-Manage.vue";
+import InspectionTask from "@/pages/Inspection-Task.vue";
 
 const router = new VueRouter({
 	routes: [
@@ -30,6 +31,12 @@ const router = new VueRouter({
 					path: 'deviceManage',
 					component: DeviceManage,
 					meta: {title: '设备概览', needAuth: true},
+				},
+				{
+					name: 'inspectionTask',
+					path: 'inspectionTask',
+					component: InspectionTask,
+					meta: {title: '巡检计划', needAuth: true},
 				}
 			]
 		}

8cbeca04fe81c0e890de705572067b00c57e3e42
junuo-S <3236877923@qq.com>
2023-04-13 20:21:23 +0800

    [日期:2023.4.13]-[项目:database]-[模块:tb_matter_query.sql]-[改动:设计物料数据表并添加一些数据]

---
+++ database/tb_matter_query.sql
@@ -0,0 +1,24 @@
+create table if not exists tb_matter(
+    id int primary key auto_increment,
+    time int not null ,
+    belt_id varchar(20) not null ,
+    -- 0:残次品 1:良好
+    quality int not null ,
+    width int not null ,
+    height int not null ,
+    info varchar(50)
+);
+
+insert into tb_matter values (1, 1681387708, 'belt-1', 1, 30, 10, '表面光滑');
+insert into tb_matter values (2, 1681387708, 'belt-2', 1, 32, 9, '表面略显粗糙');
+insert into tb_matter values (3, 1681387708, 'belt-3', 0, 31, 11, '表面有很多斑点');
+insert into tb_matter values (4, 1681387708, 'belt-1', 1, 29, 10, '带刺');
+insert into tb_matter values (5, 1681387708, 'belt-2', 1, 28, 13, '高度偏高');
+insert into tb_matter values (6, 1681387708, 'belt-3', 0, 27, 13, '长度过短，高度偏高');
+
+insert into tb_matter values (null, 1681387708, 'belt-1', 1, 30, 10, '表面光滑');
+insert into tb_matter values (null, 1681387708, 'belt-2', 1, 32, 9, '表面略显粗糙');
+insert into tb_matter values (null, 1681387708, 'belt-3', 0, 31, 11, '表面有很多斑点');
+insert into tb_matter values (null, 1681387708, 'belt-1', 1, 29, 10, '带刺');
+insert into tb_matter values (null, 1681387708, 'belt-2', 1, 28, 13, '高度偏高');
+insert into tb_matter values (null, 1681387708, 'belt-3', 0, 27, 13, '长度过短，高度偏高');

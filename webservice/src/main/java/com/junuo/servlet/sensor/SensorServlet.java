ad31caafafa31ddda14ec24e114772d90ade8f9f
junuo-S <3236877923@qq.com>
2023-04-21 16:42:50 +0800

    [日期:2023.4.21]-[项目:webservice]-[模块:Sensor]-[改动:添加SensorServlet]

---
+++ webservice/src/main/java/com/junuo/servlet/sensor/SensorServlet.java
@@ -0,0 +1,9 @@
+package com.junuo.servlet.sensor;
+
+import com.junuo.servlet.BaseServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/sensor/*")
+public class SensorServlet extends BaseServlet {
+}

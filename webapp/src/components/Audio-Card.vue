4c62ef22fcb2727e587dc4dd27c04231c52a5290
junuo-S <3236877923@qq.com>
2023-04-22 19:19:16 +0800

    [日期:2023.4.22]-[项目:webapp]-[模块:Sensor/Audio-Part]-[改动:基本完成音频波形展示]

---
+++ webapp/src/components/Audio-Card.vue
@@ -0,0 +1,89 @@
+<template>
+	<div id="peak-container">
+		<div id="zoomview-container" ref="zoomview"></div>
+		<div id="overview-container" ref="overview"></div>
+		<audio ref="audio" id="audio" muted>
+			<source src="../asset/花僮-笑纳.mp3" type="audio/mpeg" />
+		</audio>
+		<el-button
+			type="info"
+			:icon="icon"
+			circle
+			id="btn-play"
+			ref="btn-play"
+			@click="play"
+			v-show="peaks"
+		>
+		</el-button>
+	</div>
+</template>
+
+<script>
+import Peaks from "peaks.js";
+export default {
+	name: 'Audio-Card',
+	data() {
+		return {
+			icons: {play: 'el-icon-video-play', pause: 'el-icon-video-pause'},
+			playStatus: false,
+			peaks: null,
+		}
+	},
+	computed: {
+		icon() {
+			return this.playStatus ? this.icons.pause : this.icons.play;
+		},
+	},
+	created() {
+		this.$nextTick(this.init);
+	},
+	methods: {
+		init() {
+			const options = {
+				zoomview: {
+					container: this.$refs.zoomview, //缩略图
+				},
+				overview: {
+					container: this.$refs.overview,
+				},
+				mediaElement: this.$refs.audio,
+				webAudio: {
+					audioContext: new AudioContext(),
+				},
+				emitCueEvents: true,
+			};
+			Peaks.init(options, (err, peaks) => {
+				if (err) return;
+				this.peaks = peaks;
+			});
+		},
+		play() {
+			this.playStatus ? this.peaks.player.pause() : this.peaks.player.play();
+			this.playStatus = !this.playStatus;
+		}
+	}
+};
+</script>
+
+<style>
+#peak-container {
+	/*width: 30%;*/
+	/*border: lightskyblue 1px solid;*/
+	/*border-radius: 8px;*/
+	position: relative;
+}
+#btn-play {
+	position: absolute;
+	left: 3px;
+	bottom: 3px;
+}
+#audio {
+	/*display: none;*/
+}
+#zoomview-container {
+	height: 100px;
+}
+#overview-container {
+	height: 100px;
+}
+</style>
---
+++ webapp/src/components/Audio-Part.vue
@@ -0,0 +1,31 @@
+<template>
+<el-row class="audio-part">
+	<el-col :span="1">
+		<SensorBanner title="音频数据"/>
+	</el-col>
+	<el-col :span="23">
+		<AudioCard/>
+	</el-col>
+</el-row>
+</template>
+
+<script>
+import SensorBanner from "@/components/Sensor-Banner.vue";
+import AudioCard from "@/components/Audio-Card.vue";
+export default {
+	name: "Audio-Part",
+	components: {SensorBanner, AudioCard}
+}
+</script>
+
+<style scoped>
+.audio-part {
+	margin-top: 15px;
+	display: flex;
+	align-items: center;
+	height: 30%;
+	overflow: auto;
+	border: #ccc 1px solid;
+	border-radius: 8px;
+}
+</style>
